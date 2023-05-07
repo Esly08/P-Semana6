c8fccfcb8e9f84400c262b037bd0d3914b41dbaa
Esly_08 A <125147470+Esly08@users.noreply.github.com>
2023-05-06 23:06:18 -0600

    Año bisiesto.
    El programa solicita al usuario que ingrese un año, este evalúa si es un año bisiesto o no.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Por favor introduzca un año");
+int anio = int.Parse(Console.ReadLine());
+
+int divisor = 4;
+
+if (anio <= 0)
+{
+    //False
+    Console.WriteLine("El año no puede ser menor a 0");
+}
+
+if(anio > 0)
+{
+    if (anio % divisor == 0 || anio % 100 ==0 || anio % 400 == 0)
+    {
+        //True
+        Console.WriteLine("El año es biciestro");
+    }
+    else
+    {
+        //False
+        Console.WriteLine("El año no es biciestro");
+    }
+}       
+
